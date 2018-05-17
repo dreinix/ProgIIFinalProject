4328b0697b8af5b2e26e22bba6d24de41143a664
dreinix <ricardo.aar@outlook.com>
2018-05-17 13:16:07 -0400

    Start

---
+++ ProgIIFinalProject/ProgIIFinalProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIIFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("hola mundo");
+
+            Console.ReadKey();
+        }
+    }
+}

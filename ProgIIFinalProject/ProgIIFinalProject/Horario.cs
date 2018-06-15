5b02093ec0068c67da2883b5cd1d3e557330a645
Yordi Ogando <yordiogando@gmail.com>
2018-06-15 17:46:59 -0400

    Clase Programaciones y Clase Horario

---
+++ ProgIIFinalProject/ProgIIFinalProject/Horario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIIFinalProject
+{
+    class Horario
+    {
+
+        private String _dia;
+        private String _hora;
+
+
+        public string dia
+        {
+            get { return _dia; }
+            set { _dia = value; }
+        }
+
+        public string hora
+        {
+            get { return _hora; }
+            set { _hora = value; }
+        }
+    }
+}
---
+++ ProgIIFinalProject/ProgIIFinalProject/Programaciones.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIIFinalProject
+{
+    class Programaciones
+    {
+
+        private MateriasCS _materia;
+        private String _estudiante;
+        private Horario _horario;
+        private String _trimestre;
+        private String _aula;
+        private String _profesor;
+        private int _ID;
+
+        public MateriasCS materia
+        {
+            get { return _materia; }
+            set { _materia = value; }
+        }
+
+        public Horario horario
+        {
+            get { return _horario; }
+            set { _horario = value; }
+        }
+
+
+        public string estudiante
+        {
+            get { return _estudiante; }
+            set { _estudiante = value; }
+        }
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        public string trimestre
+        {
+            get { return _trimestre; }
+            set { _trimestre = value; }
+        }
+        public string aula
+        {
+            get { return _aula; }
+            set { _aula = value; }
+        }
+
+
+        public string profesor
+        {
+            get { return _profesor; }
+            set { _profesor = value; }
+        }
+
+        public void GenerarID()
+        {
+            string iD = "";
+            Random r1 = new Random();
+            iD = "" + r1.Next(0000, 10000);
+            _ID = int.Parse(iD);
+        }
+
+    }
+}
